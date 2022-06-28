f85d254459d7e12155e517efa5756f4eb477aec0
Snehan Kekre <snehan.minerva@gmail.com>
2022-06-28 16:27:29 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import subprocess
+import sys
+import os
+
+if not os.path.isdir("pretrained"):
+  subprocess.call("bash setup.sh", shell=True)
+  
+with st.form("min(DALL.E)"):
+    prompt = st.text_input("Enter your text here:")
+    submit = st.form_submit_button("Generate")
+
+if submit:
+    subprocess.call(
+        f"{sys.executable} image_from_text.py --text='{prompt}' --seed=7", shell=True
+    )
+
+    st.image("generated.png")
